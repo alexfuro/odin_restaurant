1ec55458b4750406c70a6f5f96663409535511b8
AlexFuro <afurones43@gmail.com>
2018-06-20 12:39:17 -0400

    Add basic contact module

---
+++ src/contact.js
@@ -0,0 +1,22 @@
+const contact_page = () => {
+  const heading = () => {
+    let newHeading = document.createElement("h1");
+    newHeading.innerText = "Contact Us";
+    return newHeading;
+  };
+  const contact_info = () => {
+    let newInfo = document.createElement("p");
+    newInfo.innerText = "La Esquina can be contacted at: Twitter: @laesquina";
+    return newInfo;
+  };
+  const DOMmanip = () =>{
+    let container = document.getElementById("content");
+    container.setAttribute("style", "background: #a71c0f;");
+    container.appendChild(heading());
+    container.appendChild(contact_info());
+    return true;
+  };
+  return DOMmanip() ;
+};
+
+export default contact_page;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import home_page from './home_page';
 import tabs from './tabs';
 import clearDOM from './clearDOM'
+import contact from './contact'
 
 let tabLabels = ["Home","Contact","Menu"];
 tabs(tabLabels);
 home_page();
 clearDOM();
+contact();
